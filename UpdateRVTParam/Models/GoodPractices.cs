8ad86ce640483e6dbd13bc6745fdaae6f3132eed
andrea@atbim.es <andrea@atbim.es>
2020-11-20 14:29:36 +0100

    Orden del codigo Añadir listas Añadir más recuentos

--- UpdateRVTParam/Models/GoodPractices.cs
+++ UpdateRVTParam/Models/GoodPractices.cs
@@ -1,6 +1,7 @@
 ﻿using Autodesk.Revit.DB;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace UpdateRVTParam.Models
@@ -8,6 +9,8 @@ namespace UpdateRVTParam.Models
     public class GoodPractices
     {
         private Document _doc;
+
+        #region Variables
         public int nAvisos { get; set; }
         public int nVistas { get; set; }
         public int dwgT { get; set; }
@@ -45,18 +48,31 @@ namespace UpdateRVTParam.Models
         public int legendsT { get; set; }
         public int draftingViewT { get; set; }
         public int textT { get; set; }
+        public int assemblyT { get; set; }
+        #endregion
+
+        #region Listas
+        public List<string> wallTypes { get; set; }
+        public List<string> floorTypes { get; set; }
+
+        public List<string> ceilingsTypes { get; set; }
+        public List<string> roofTypes { get; set; }
+        public List<string> railingTypes { get; set; }
+        public List<string> worksetsNames { get; set; }
+        public List<string> pipesystem { get; set; }
+        public List<string> BrowserOrganization { get; set; }
+        #endregion
 
         public GoodPractices(Document doc)
         {
             this._doc = doc;
+
+            #region General Information
+            //Recuentos de elementos
             this.nAvisos = doc.GetWarnings().Count;
             this.nVistas = CountInstancesFromDocument(BuiltInCategory.OST_Views);
             this.dwgT = CountTypesFromDocument(typeof(CADLinkType));
-            this.familiesC = CountInstancesFromDocument(typeof(Family));
             this.groupsM = CountInstancesFromDocument(typeof(Group));
-            this.genericModel = CountInstancesFromDocument(BuiltInCategory.OST_GenericModel);
-            this.floorsT = CountInstancesFromDocument(BuiltInCategory.OST_Floors);
-            this.wallsT = CountInstancesFromDocument(BuiltInCategory.OST_Walls);
             this.optionsT = CountInstancesFromDocument(BuiltInCategory.OST_DesignOptions);
             this.worksetsT = new FilteredWorksetCollector(doc).OfKind(WorksetKind.UserWorkset).Count();
             this.levelsT = CountInstancesFromDocument(typeof(Level));
@@ -66,10 +82,29 @@ namespace UpdateRVTParam.Models
             this.sheetsT = CountInstancesFromDocument(BuiltInCategory.OST_Sheets);
             this.templatesViewT = IntancesFromDocument(typeof(View)).Cast<View>().Where(x => x.IsTemplate == true).Count();
             this.schedulesT = CountInstancesFromDocument(typeof(ViewSchedule));
-
             this.imagesT = CountInstancesFromDocument(typeof(ImageType)); //Revisar, no se exactamente que devuelve el Image Type
             this.rvtT = CountTypesFromDocument(typeof(RevitLinkInstance));
-            this.areasT = CountTypesFromDocument(typeof(Area)); //Revisar que devuelve, no estoy segura de que sean las areas
+            this.areasT = CountTypesFromDocument(typeof(Area)); //Revisar que devuelve
+            this.linepatternT = CountTypesFromDocument(typeof(LinePattern));
+            this.dimensionT = CountTypesFromDocument(typeof(Dimension));
+            this.scopeBox = CountInstancesFromDocument(BuiltInCategory.OST_VolumeOfInterest);
+            this.legendsT = CountInstancesFromDocument(BuiltInCategory.OST_LegendComponents);
+            this.draftingViewT = CountInstancesFromDocument(typeof(ViewDrafting));
+            this.textT = CountInstancesFromDocument(BuiltInCategory.OST_TextNotes);
+            this.assemblyT = CountInstancesFromDocument(typeof(AssemblyInstance));//Revisar si devuelve información correcta
+
+            //Listas de elementos
+            this.worksetsNames = new FilteredWorksetCollector(doc).OfKind(WorksetKind.UserWorkset).Select(wn => wn.Name).ToList();
+            this.BrowserOrganization = new FilteredElementCollector(doc).OfClass(typeof(BrowserOrganization)).Select(bo => bo.Name).ToList();
+
+            #endregion
+
+            #region Architecture
+            //Recuentos de elementos
+            this.familiesC = CountInstancesFromDocument(typeof(Family));
+            this.genericModel = CountInstancesFromDocument(BuiltInCategory.OST_GenericModel);
+            this.floorsT = CountInstancesFromDocument(BuiltInCategory.OST_Floors);
+            this.wallsT = CountInstancesFromDocument(BuiltInCategory.OST_Walls);
             this.ceilingT = CountInstancesFromDocument(BuiltInCategory.OST_Ceilings);
             this.railingT = CountInstancesFromDocument(BuiltInCategory.OST_Railings);
             this.roofT = CountInstancesFromDocument(BuiltInCategory.OST_Roofs);
@@ -77,18 +112,33 @@ namespace UpdateRVTParam.Models
             this.stairsT = CountInstancesFromDocument(BuiltInCategory.OST_Stairs);
             this.rampT = CountInstancesFromDocument(BuiltInCategory.OST_Ramps);
             this.curtainWall = CountInstancesFromDocument(BuiltInCategory.OST_CurtainWallPanels);
+            
+            //Listas de elementos
+            this.wallTypes = new FilteredElementCollector(doc).WhereElementIsElementType().OfCategory(BuiltInCategory.OST_Walls).Select(mn => mn.Name).ToList();
+            this.ceilingsTypes = new FilteredElementCollector(doc).WhereElementIsElementType().OfCategory(BuiltInCategory.OST_Ceilings).Select(cn => cn.Name).ToList();
+            this.floorTypes = new FilteredElementCollector(doc).WhereElementIsElementType().OfCategory(BuiltInCategory.OST_Floors).Select(fn => fn.Name).ToList();
+            this.railingTypes = new FilteredElementCollector(doc).WhereElementIsElementType().OfCategory(BuiltInCategory.OST_Railings).Select(fn => fn.Name).ToList();
+            this.roofTypes = new FilteredElementCollector(doc).WhereElementIsElementType().OfCategory(BuiltInCategory.OST_Roofs).Select(fn => fn.Name).ToList();
+            #endregion
+
+            #region MEP
+            //Recuentos de elementos
             this.ductsT = CountInstancesFromDocument(BuiltInCategory.OST_DuctCurves);
             this.pipesT = CountInstancesFromDocument(BuiltInCategory.OST_PipeSegments);
-            this.linepatternT = CountTypesFromDocument(typeof(LinePattern));
-            this.dimensionT = CountTypesFromDocument(typeof(Dimension));
-            this.scopeBox = CountInstancesFromDocument(BuiltInCategory.OST_VolumeOfInterest);
-            this.legendsT = CountInstancesFromDocument(BuiltInCategory.OST_LegendComponents);
-            this.draftingViewT = CountInstancesFromDocument(typeof(ViewDrafting));
-            this.textT = CountInstancesFromDocument(BuiltInCategory.OST_TextNotes);
+            //Listas de elementos
+            #endregion
 
-            //Añadidos Andrea
+            #region Structure
+            //Recuentos de elementos
             this.columnsT = CountInstancesFromDocument(BuiltInCategory.OST_StructuralColumns);
             this.framingT = CountInstancesFromDocument(BuiltInCategory.OST_StructuralFraming);
+
+            //Listas de elementos
+
+            #endregion
+
+
+
         }
 
         private int CountInstancesFromDocument(BuiltInCategory bic)
